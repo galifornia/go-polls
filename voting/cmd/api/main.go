93e0e6f914fe2d62754b97cad0bb9a3b0275f64c
galifornia <martinho87@gmail.com>
2022-08-18 23:06:09 +0200

    [voting] add CORS, Logger & Recover middleware

--- voting/cmd/api/main.go
+++ voting/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 const PORT = "80"
@@ -13,6 +14,12 @@ func main() {
 	app := App{}
 
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	app.setupRoutes(e)
 
 	e.Logger.Fatal(e.Start(":" + PORT))
